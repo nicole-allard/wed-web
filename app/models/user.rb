c000c383cc65926aec0db6e13ef0989d8102ecab
Nicole <nicole@rapleaf.com>
2012-09-15 15:55:23 -0700

    adding user table and guest associations

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ActiveRecord::Base
+  has_one :guest_assoc
+  has_one :guest_user, :through => :guest_assoc, :source => :guest
+  has_one :inverse_guest_assoc, :class_name => "GuestAssoc", :foreign_key => :guest_id
+  has_one :inverse_guest_user, :through => :inverse_guest_assoc, :source => :user
+  
+  def guest
+    guest_user || inverse_guest_user
+  end
+end
