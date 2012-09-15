4ad84df88a3af07c899aa10f4a49fac90c95317e
Nicole <nicole@rapleaf.com>
2012-09-15 13:30:50 -0700

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WebWeb::Application.config.secret_token = '002ba2baf13cfe770c30da81ac6a4c89a1c6855b26912634cc4885059d813f72f320046649a97d7b3e5751636e33fbb18ccd8c7aa995f8f529f3e21856331bc6'
