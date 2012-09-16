3a5c0075e1bdf2d803b0d3c975a63188c50190ee
Ken Struys <ken@struys.ca>
2012-09-15 20:33:40 -0700

    got header hack thing working

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -4,38 +4,49 @@ $(function() {
 	var $window = $(window);
 	var FADE_AFTER_SCROLL_TOP = 400;
 	var FIXED_POSITION_AFTER_SCROLL_TOP = 660;
+	var inFixedMode = false;
+   		
+	var headerHeight = $('.header').height();
+	
+	// $('.main-container').css({
+		// 'margin-top': headerHeight
+	// });
 	
 	$window.on('scroll', function() {
    		
    		var scrollTop = $(this).scrollTop();
    		
-   		var hackDiv = $('div.hackDiv').hide();
-   		hackDiv.appendTo('body');
-   		
-   		hackDiv.css({
-   			height: $('.header').height()	
-   		})
    		
    		var relativeScrollTop = scrollTop - FADE_AFTER_SCROLL_TOP;
    		
-   			$('.tree').css({
-   				opacity : 1 - relativeScrollTop/100
-   			});
-   		
-   			if(scrollTop > FIXED_POSITION_AFTER_SCROLL_TOP) {
-   				$('.header').css({
-   					position: 'fixed',
-   					top: '-669px'
-   				});
-   				hackDiv.show();
-   			} else {
-   				$('.header').css({
-   					position: '',
-   					top: ''
-   				})
-   				hackDiv.hide();
-   			}
-   		
+		$('.tree').css({
+			opacity : 1 - relativeScrollTop/100
+		});
+		
+		
+		
+		if (scrollTop > FADE_AFTER_SCROLL_TOP) {
+			$('.navigation-hack').css('background-color', '#F1F1F1');
+		} else {
+			$('.navigation-hack').css('background-color', '');	
+		}
+		
+			
+		if(scrollTop > FIXED_POSITION_AFTER_SCROLL_TOP) {
+			$('.navigation-hack').css({
+				position: 'fixed',
+				top:0,
+			});
+			
+			var height = $('.navigation-hack').height();
+			$('.main-container').css('margin-top', height);
+		} else {
+			$('.navigation-hack').css({
+				position: '',
+				top:'',
+			});
+			$('.main-container').css('margin-top', '');
+		}
 	});
 	
 });
