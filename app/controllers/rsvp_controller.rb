0eed9001237ac3ef0e37209039ab0b3dab2b0d47
Nicole <nicole@rapleaf.com>
2012-11-04 22:18:40 -0800

    showing success/err/info messages in rsvp form

--- app/controllers/rsvp_controller.rb
+++ app/controllers/rsvp_controller.rb
@@ -5,7 +5,6 @@ class RsvpController < ApplicationController
   end
 
   def update_rsvp
-    puts "\n\n\n#{params.inspect}\n\n\n"
     @active_user.update_attributes({:email => params["user_email"], :name => params["user_name"], :status => params["user_status"]})
     user_errors = @active_user.errors
     
@@ -16,9 +15,11 @@ class RsvpController < ApplicationController
     end
     
     if !user_errors.blank? || !guest_errors.blank?
-      # todo return errs
+      @errors = user_errors.values + guest_errors.values
+    else
+      @success = "Successfully updated attendee information"
     end
-    # todo return success
-    redirect_to (:action => index)
+    
+    render "index"
   end
 end
