8ecf59e063bb4b8454521491bd14a52f5e022af1
Ken Struys <ken@struys.ca>
2012-11-06 22:30:58 -0800

    added clearfix styles and update map markers

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -1,21 +1,41 @@
 $(function() {
 	var mapOptions = {
-      center: new google.maps.LatLng(37.7837486, -122.4077904),
       zoom: 14,
       mapTypeId: google.maps.MapTypeId.ROADMAP
     };
-	var map = new google.maps.Map(document.getElementById("map_canvas"),
-        mapOptions);
+    
+	var map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
             
 	var homeMarker = new google.maps.Marker({
    		'map': map, 
    		'position': new google.maps.LatLng(37.79083, -122.409154),
-   		'title': 'Our Appartment'
+   		'title': 'Our Appartment',
+   		'icon': new google.maps.MarkerImage(
+   			'http://maps.google.com/intl/en_us/mapfiles/ms/micons/green.png'
+   		)
    	});
 
 	var venuMarker = new google.maps.Marker({
    		'map': map,
    		'position': new google.maps.LatLng(37.761836, -122.422917),
-   		'title': 'Farina Focaccia & Cucina Italiana'
+   		'title': 'Farina Focaccia & Cucina Italiana',
+   		'icon': new google.maps.MarkerImage(
+   			'http://maps.google.com/intl/en_us/mapfiles/ms/micons/red.png'
+   		)
    	});
+   	
+   	var hotelMayFlower = new google.maps.Marker({
+   		'map': map,
+   		'position': new google.maps.LatLng(37.789308, -122.413283),
+   		'title': 'The Hotel Mayflower',
+   		'icon': new google.maps.MarkerImage(
+   			'http://maps.google.com/intl/en_us/mapfiles/ms/micons/blue.png'
+   		)
+   	})
+   	
+   	var bounds = 
+   		(new google.maps.LatLngBounds(homeMarker.getPosition(), homeMarker.getPosition()))
+   		.union(new google.maps.LatLngBounds(venuMarker.getPosition(), venuMarker.getPosition()));
+   			
+   	map.fitBounds(bounds);
 });
