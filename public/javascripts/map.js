d39f3a7fe613cd46c7b3d343221357a0121d4f00
Nicole <nicole@rapleaf.com>
2012-11-17 13:37:49 -0800

    updated map markers

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -5,13 +5,18 @@ $(function() {
     };
     
 	var map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
-            
+        var size = [500, 600]
+	
 	var homeMarker = new google.maps.Marker({
    		'map': map, 
    		'position': new google.maps.LatLng(37.79083, -122.409154),
    		'title': 'Our Appartment',
    		'icon': new google.maps.MarkerImage(
-   			'http://maps.google.com/intl/en_us/mapfiles/ms/micons/green.png'
+   			'/images/map-pin-home.png',
+			null,
+			null,
+			null,
+			new google.maps.Size(33, 40)
    		)
    	});
 
@@ -20,7 +25,11 @@ $(function() {
    		'position': new google.maps.LatLng(37.761836, -122.422917),
    		'title': 'Farina Focaccia & Cucina Italiana',
    		'icon': new google.maps.MarkerImage(
-   			'http://maps.google.com/intl/en_us/mapfiles/ms/micons/red.png'
+   			'/images/map-pin-venue.png',
+			null,
+			null,
+			null,
+			new google.maps.Size(33, 40)
    		)
    	});
    	
@@ -29,7 +38,11 @@ $(function() {
    		'position': new google.maps.LatLng(37.789308, -122.413283),
    		'title': 'The Hotel Mayflower',
    		'icon': new google.maps.MarkerImage(
-   			'http://maps.google.com/intl/en_us/mapfiles/ms/micons/blue.png'
+   			'/images/map-pin-hotel.png',
+			null,
+			null,
+			null,
+			new google.maps.Size(33, 40)
    		)
    	})
    	
