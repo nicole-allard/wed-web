42cc22bf03b2b4000c8e4899d7b193e336d4440c
Ken Struys <ken@struys.ca>
2013-08-25 18:30:23 -0700

    styled registry

---
+++ public/javascripts/registry.js
@@ -0,0 +1,19 @@
+
+$(function() {
+	$('form').submit(function(e) {
+		e.preventDefault();
+		var items = $('li[data-item-id]').map(function() {
+			return $(this).data('item-id') + escape('=')  + $(this).find('select').val();
+		});
+		
+		// Somewhat hacky way of making a real form submit
+		$('<form>')
+			.attr({
+				'action': '/registry/update_reservations?item-counts=' + items.toArray().join('&'),
+				'method': 'post'
+			})
+			.appendTo('body')
+			.submit();
+	});
+});
+
